3740ea0d1c96277762d5a03a462bbe0ecb48d4cb
wukaifei <2115726343@qq.com>
2018-08-24 18:55:45 +0800

    基础内容

---
+++ base/base_driver.py
@@ -0,0 +1,14 @@
+from appium import webdriver
+
+
+def init_driver():
+    desired_caps = {}
+    desired_caps['platformName'] = 'android'
+    desired_caps['platformVersion'] = '5.1'
+    desired_caps['deviceName'] = '192.168.49.101:5555'
+    desired_caps['unicodeKeyboard'] = True
+    desired_caps['resetKeyboard'] = True
+    # app 信息
+    desired_caps['appPackage'] = 'com.android.settings'
+    desired_caps['appActivity'] = '.Settings'
+    return webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
